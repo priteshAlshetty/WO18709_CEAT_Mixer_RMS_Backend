b23b1fa7adefa82dd101c3c78c645cf9e4dff75e
priteshAlshetty <alshettypritesh4@gmail.com>
2025-10-29 11:13:57 +0530

    "error handling done for add material routes for missing fields"

--- routes/material.routes.js
+++ routes/material.routes.js
@@ -20,6 +20,12 @@ router.get('/getMaterials', async (req, res) => {
 // Route to add a new material
 router.post('/addMaterial', async (req, res) => {
     const materialData = req.body.material_data;
+
+    if (!materialData || !materialData.material_code || !materialData.material_name || !materialData.material_type) {
+        return res.status(400).json({ 
+            errLocation :"addMaterial route - validation",
+            error: "missing fields: material data. 'material_code', 'material_name', and 'material_type' are required." });
+    }
     const result = await addMaterial(materialData);
     if (result.success) {
         res.status(201).json(result);
